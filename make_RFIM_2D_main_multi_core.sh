1a866df89c0d4b585dc61c09094d9e884713bdee
sanket deshpande <teknas97@gmail.com>
2017-11-26 02:02:24 +0530

    changed name to main_RFIM_2D_main_multi_core.sh
    script to run RFIM_2D_main_multi_core.cpp code

--- make_RFIM_2D_main_multi_core.sh
+++ make_RFIM_2D_main_multi_core.sh
@@ -2,10 +2,12 @@
 #obj_file=(${1//./ })
 
 if  g++ --std=c++14 -Wall RFIM_2D_main_multi_core.cpp LatticeStuff.cpp  Clustering.cpp PushRelabel.cpp -lpthread -o main ; then 
-    echo '*************'
+    echo '****** RUNNING MULTI CORE RFIM SIMULATION *******'
     ./main
     
 else 
+    echo '  XXXXX  SOMETHING WENT WRONG XXXXX '
+    echo '  XXXXX  most probably your career decision '
     exit
 fi
 
