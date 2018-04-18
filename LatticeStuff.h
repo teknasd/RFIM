50ed4ea8d838742b443e97cd2ae4d197816e515d
sanket deshpande <teknas97@gmail.com>
2018-04-18 16:37:26 +0530

    minor change

--- LatticeStuff.h
+++ LatticeStuff.h
@@ -8,7 +8,7 @@ void update_Bmat_bimodal(std::vector <float>& Bmat, float del);
 
 void create_Bmat_gaussian(std::vector <float>& Bmat, float del);
 
-void create_Exmat(std::vector < std::vector <int> >& Exmat, std::vector <int> & latt);
+void create_Exmat(std::vector < std::vector <int> >& Exmat);
 
 void create_CapacityMat(std::vector < std::vector <float> >& CapacityMat, std::vector < std::vector <int> >& Exmat);
 
@@ -20,9 +20,32 @@ void create_Residual_graph(std::vector < std::vector <float> >& CapacityMat, std
 
 void printMatrix(std::vector <std::vector <int> > const & M, int len);
 
+void printMatrix(std::vector <std::vector <float> >  & M, int len);
+
 void printMatrix(std::vector<int> const & M, int len);
 
 void savedata(std::vector< int > & Mat, int l, float del, std::string s);
 
+void savedata(std::vector< std::vector< float > > & Mat, int l, float del, std::string s);
+
 void savedata(std::vector< float > & Mat, int l, float del, std::string s);
 
+// this func re- intializes all the matrices with zero.
+void Reinit(std::vector < std::vector <int> >&   sqlat0,
+	std::vector < std::vector <int> >& sqlat1,
+	std::vector < std::vector <float> >&   flow,
+	std::vector < std::vector<float> >& CapacityMat,
+	std::vector <int>&  visited,
+	std::vector <int>&  clusstats0,
+	std::vector <int>&  clusstats1,
+	std::vector<float>& Wmat);
+
+// this func clears all matrices
+void ClearAll(std::vector < std::vector <int> >&   sqlat0,
+	std::vector < std::vector <int> >& sqlat1,
+	std::vector < std::vector <float> >&   flow,
+	std::vector < std::vector<float> >& CapacityMat,
+	std::vector <int>&  visited,
+	std::vector <int>&  clusstats0,
+	std::vector <int>&  clusstats1,
+	std::vector<float>& Wmat);
